caf620a1cf7f745c339b4fe5b2ceb7cfce471544
bjorgvin <bjorgvin.thordarson@gmail.com>
2014-04-27 16:43:07 +0000

    print message on callback

--- lib/omniauth/strategies/islykill.rb
+++ lib/omniauth/strategies/islykill.rb
@@ -1,7 +1,14 @@
 module OmniAuth
   module Strategies
-    class Islykill
-      include OmniAuth::Strategy::SAML
+    class Islykill < OmniAuth::Strategy::SAML
+        def callback_phase
+puts "   ___      _ _ _                _    "
+puts "  / __ __ _| | | |__   __ _  ___| | __"
+puts " / /  / _` | | | '_   / _` |/ __| |/ /"
+puts "/ /__| (_| | | | |_) | (_| | (__|   < "
+puts " ____/ __,_|_|_|_.__/  __,_| ___|_| _ "
+puts "                                      "
+        end
     end
   end
 end
